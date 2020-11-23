937364676662587798359fcb53e4c66d3c52b7f0
la-rua <thishzw@hotmail.com>
2020-11-23 11:01:25 +0800

    muti_thread

--- slots.py
+++ slots.py
@@ -7,14 +7,20 @@
 # ++                                 la-rua      ++
 # +++++++++++++++++++++++++++++++++++++++++++++++++
 import weakref
+from threading import RLock
 class CommonSlots (object):
     objectRef = {}
+    objectRLock = RLock()
+    #objectRef:{id:(ref,times)}
     slots = {}
+    slotsRLock = RLock()
+    #slots{(id,signal):(id,slot)}
     classOrdDelFun = {}
-
+    #classOrdDelFun{className:(__del__)}
 def addSlot(srcObject = None,signal ="",dstObject = None,slot = lambda : None ):
     srcId = -1
     dstId = -1
+    CommonSlots.objectRLock.acquire()
     if srcObject != None:
         srcId = id(srcObject)
         setattr( type(srcObject),"__del__",returnDelFun(srcObject))
@@ -25,14 +31,18 @@ def addSlot(srcObject = None,signal ="",dstObject = None,slot = lambda : None ):
         CommonSlots.objectRef[srcId] = weakref.ref(srcObject)
     if not dstId in CommonSlots.objectRef:
         CommonSlots.objectRef[dstId] = weakref.ref(dstObject)
+    CommonSlots.objectRLock.release()
+    CommonSlots.slotsRLock.acquire()
     if not (srcId,signal) in CommonSlots.slots:
         CommonSlots.slots[(srcId,signal)] = set()
     funSet = CommonSlots.slots[(srcId,signal)]
     CommonSlots.slots[(srcId,signal)] = funSet | set([(dstId,slot),])
+    CommonSlots.slotsRLock.release()
 
 def delSlot(srcObject = None,signal = None,dstObject = None,slot =  lambda : None):
     srcId = -1
     dstId = -1
+    CommonSlots.objectRLock.acquire()
     if srcObject != None:
         srcId = id(srcObject)
         fun = returnOrdDelFun(srcObject)
@@ -48,15 +58,21 @@ def delSlot(srcObject = None,signal = None,dstObject = None,slot =  lambda : Non
         else:
             setattr(type(dstObject),"__del__",fun)
     if not srcId in CommonSlots.objectRef:
+        CommonSlots.objectRLock.release()
         return
     if not dstId in CommonSlots.objectRef:
+        CommonSlots.objectRLock.release()
         return
+    CommonSlots.objectRLock.release()
+    CommonSlots.slotsRLock.acquire()
     if not (srcId,signal) in CommonSlots.slots:
+        CommonSlots.slotsRLock.release()
         return
     funSet = CommonSlots.slots[(srcId,signal)]
     CommonSlots.slots[(srcId,signal)] = funSet - set([(dstId,slot),])
+    CommonSlots.slotsRLock.release()
 
-def Signal(self,signal):
+def Signal(self,signal,*args,**kw):
     srcId = -1
     if self != None:
         srcId = id(self)
@@ -68,12 +84,12 @@ def Signal(self,signal):
     for song in funSet:
         dstId,f = song
         if dstId == -1:
-            f()
+            f(*args,**kw)
         else:
             if not dstId in CommonSlots.objectRef:
                 return
             dstObject = CommonSlots.objectRef[dstId]
-            f(dstObject())
+            f(dstObject(),*args,**kw)
 
 def returnDelFun(self):
     fun = None
